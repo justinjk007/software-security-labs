c3434985fb496a718e5defd73cff56b3f042dbff
Justin K -Linux <justinjoseph0007@gmail.com>
2020-02-10 21:35:50 -0500

    Finished the report

--- lab 2/Task 2/use_cap.c
+++ lab 2/Task 2/use_cap.c
@@ -42,16 +42,24 @@ int main(void)
 {
     if (open("/etc/shadow", O_RDONLY) < 0)
         printf("(a) Open failed\n"); /*Question (a): is the above open sucessful? why?*/
+
     if (cap_disable(CAP_DAC_READ_SEARCH) < 0) return -1;
+
     if (open("/etc/shadow", O_RDONLY) < 0)
         printf("(b) Open failed\n"); /*Question (b): is the above open sucessful? why?*/
+
     if (cap_enable(CAP_DAC_READ_SEARCH) < 0) return -1;
+
     if (open("/etc/shadow", O_RDONLY) < 0)
         printf("(c) Open failed\n"); /*Question (c): is the above open sucessful? why?*/
+
     if (cap_drop(CAP_DAC_READ_SEARCH) < 0) return -1;
+
     if (open("/etc/shadow", O_RDONLY) < 0)
         printf("(d) Open failed\n"); /*Question (d): is the above open sucessful? why?*/
+
     if (cap_enable(CAP_DAC_READ_SEARCH) == 0) return -1;
+
     if (open("/etc/shadow", O_RDONLY) < 0)
         printf("(e) Open failed\n"); /*Question (e): is the above open sucessful? why?*/
 }
